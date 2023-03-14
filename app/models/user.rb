4257147620b7698b83ce762c1a194f93ffa02810
javascriptsucks <evix1988@live.cn>
2023-03-13 21:19:04 -0600

    user-model-validitor-test

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,9 @@
 
 class User < ApplicationRecord
   has_secure_password
-  validates :e_mail, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  validates :e_mail, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validates :password, length: { minimum: 7 }, presence: true
+  validates :password_confirmation, length: { minimum: 7 }, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,92 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  user = User.new(e_mail: "test@example.com", first_name: "first name", last_name: "last name", password: "password", password_confirmation: "password")
 
+  describe 'Validations' do
+
+    it "is valid with valid attributes" do
+      expect(user).to be_valid
+
+    end
+    it "is not valid without first_name" do
+      user.first_name = nil
+      expect(user).to_not be_valid
+
+    end
+    it "is not valid without last_name" do
+      user.first_name = "first name"
+      user.last_name = nil
+      expect(user).to_not be_valid
+
+    end
+    it "is not valid without e_mail" do
+      user.last_name = "last name"
+      user.e_mail = nil
+
+      expect(user).to_not be_valid
+      expect(user.errors.full_messages[0]).to include("E mail can't be blank")
+      expect(user.errors.full_messages[1]).to include("E mail is invalid")
+
+    end
+
+    it "is not valid with an invalid email format" do
+      user.e_mail = "Invalid email string@a.c"
+      expect(user).to_not be_valid
+      expect(user.errors.full_messages[0]).to include("E mail is invalid")
+    end
+
+    it "is not valid without password" do
+      user.e_mail = "test@example.com"
+      user.password = nil
+      expect(user).to_not be_valid
+      expect(user.errors.full_messages[0]).to include("Password can't be blank")
+      expect(user.errors.full_messages[1]).to include("Password is too short (minimum is 7 characters)")
+      expect(user.errors.full_messages[2]).to include("Password can't be blank")
+
+    end
+
+    it "is not valid without password_confirmation" do
+      user.password = "password"
+      user.password_confirmation = nil
+      expect(user).to_not be_valid
+      expect(user.errors.full_messages[0]).to include("Password confirmation is too short (minimum is 7 characters)")
+      expect(user.errors.full_messages[1]).to include("Password confirmation can't be blank")
+
+    end
+
+    it "is not valid with different password and password_confirmation" do
+      user.password = "password"
+      user.password_confirmation = "password1"
+      expect(user).to_not be_valid
+      expect(user.errors.full_messages[0]).to include("Password confirmation doesn't match Password")
+    end
+
+    it "is not valid with password or password_confirmation that less than 7 character" do
+      user.password = "passwo"
+      user.password_confirmation = "passwo"
+      expect(user).to_not be_valid
+      expect(user.errors.full_messages[0]).to include("Password is too short (minimum is 7 characters)")
+      expect(user.errors.full_messages[1]).to include("Password confirmation is too short (minimum is 7 characters)")
+    end
+
+    it "is not valid with duplicated email address" do
+      user.password_confirmation = "password"
+      user.password = "password"
+      user.save
+      user_duplicated = User.new e_mail: "test@example.com", first_name: "first name", last_name: "last name", password: "password", password_confirmation: "password"
+      expect(user_duplicated.valid?).to eql(false)
+      expect(user_duplicated.errors.full_messages[0]).to include("E mail has already been taken")
+    end
+
+
+    it "is not valid with duplicated email address and case sensitive false" do
+      user = User.new(e_mail: "test@example.com", first_name: "first name", last_name: "last name", password: "password", password_confirmation: "password")
+      user.save
+      user_duplicated = User.new e_mail: "TeSt@example.CoM", first_name: "first name", last_name: "last name", password: "password", password_confirmation: "password"
+      expect(user_duplicated.valid?).to eql(false)
+      expect(user_duplicated.errors.full_messages[0]).to include("E mail has already been taken")
+    end
+
+  end
 end
